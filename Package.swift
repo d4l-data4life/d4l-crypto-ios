4bb3f727ca3d4e9b7826a2e3053282f0786d2290
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-09-03 16:48:39 +0200

    Update release checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Data4LifeCryptoRSAPSS",
-                 url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-crypto-ios/Data4LifeCrypto-xcframework-1.7.0.zip",
+                 url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-crypto-ios/Data4LifeCrypto-xcframework-v1.7.0.zip",
                  .branch("feature/shrink-to-package-only"))
 
     ],
@@ -29,8 +29,8 @@ let package = Package(
                 path: "SwiftPMDependencyTarget/RSAPSS"),
         .binaryTarget(
             name: "Data4LifeCrypto",
-            url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-crypto-ios/Data4LifeCrypto-xcframework-1.7.0.zip",
-            checksum: "998813cdfe12061d42dc909b0510a439aaf14a6de967a858ba253cc2b8e29c57"
+            url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-crypto-ios/Data4LifeCrypto-xcframework-v1.7.0.zip",
+            checksum: "20b1c9ef61cb0ae048c4c83fbccaedc4496d783f14ca8412c6c99498e186a333"
         ),
         .testTarget(name: "Data4LifeCryptoTests",
                     dependencies: ["Data4LifeCrypto"],
