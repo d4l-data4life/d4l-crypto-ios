1e5ef990db4631af37be84a6f535ae2297cf3d67
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-11-17 15:36:42 +0100

    Preparing for the v1.8.0 release

--- Package.swift
+++ Package.swift
@@ -29,8 +29,11 @@ let package = Package(
                 path: "SwiftPMDependencyTarget/RSAPSS"),
         .binaryTarget(
             name: "Data4LifeCrypto",
-            url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-crypto-ios/Data4LifeCrypto-xcframework-v1.7.0.zip",
-            checksum: "f0b8ac45ac6294bd706ce9872738c7e3a23dd00cd3358e280f7cf322558ba3e8"
+            url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-crypto-ios/Data4LifeCrypto-xcframework-v1.8.0.zip",
+            checksum: "2021-11-17 15:36:36.590 xcodebuild[50669:11807774] [MT] DVTPlugInManager: Required plug-in compatibility UUID 8BAA96B4-5225-471B-B124-D32A349B8106 for Kotlin.ideplugin (org.kotlinlang.xcode.kotlin) not present
+2021-11-17 15:36:37.871 xcodebuild[50671:11807925] [MT] DVTPlugInManager: Required plug-in compatibility UUID 8BAA96B4-5225-471B-B124-D32A349B8106 for Kotlin.ideplugin (org.kotlinlang.xcode.kotlin) not present
+unable to restore state from /Users/alessio.borraccino/Documents/workspace/d4l-crypto-ios/.build/workspace-state.json; missingKey("path")
+7f2b7dcb6397ec26748115f8ea6cfda7d2c6a1a2c986eac230cbd8f63a9fe14b"
         ),
         .testTarget(name: "Data4LifeCryptoTests",
                     dependencies: ["Data4LifeCrypto"],
